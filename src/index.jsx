481b63f4dba67196f407117e7a8739cad932b140
Jamez <154967862+JamesH200@users.noreply.github.com>
2025-01-07 10:59:30 -0800

    update vscode

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import { createRoot } from "react-dom/client";
+// Import the main SCSS file for styling
+import "./index.scss";
 
-const App = () => {
-  return <h1>Welcome to myFlix!</h1>;
+// Main component for the app
+const MyFlixApplication = () => {
+  return (
+    <div className="my-flix">
+      <h1>Welcome to MyFlix</h1>
+      <p>Good Morning! Explore your favorite movies here.</p>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// Find the root element in the DOM
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+// Render the main application component into the root DOM element
+root.render(<MyFlixApplication />);
+
