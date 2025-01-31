2d37e26d64027aab09e4c2f75ca8d60b55861f92
Jamez <154967862+JamesH200@users.noreply.github.com>
2025-01-30 16:50:10 -0800

    update components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./movie-card.scss"; // Optional styling
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => onMovieClick(movie)}
-      style={{
-        border: "1px solid #ccc",
-        padding: "10px",
-        margin: "10px",
-        cursor: "pointer",
-      }}
-    >
-      <h2>{movie.title}</h2>
-      {movie.image && (
-        <img
-          src={movie.image}
-          alt={`${movie.title} poster`}
-          style={{ width: "100px", height: "150px", objectFit: "cover" }}
-        />
-      )}
+    <div className="movie-card" onClick={() => onMovieClick(movie)}>
+      <img src={movie.ImagePath} alt={movie.Title} className="movie-image" />
+      <h3 className="movie-title">{movie.Title}</h3>
     </div>
   );
 };
@@ -27,9 +14,10 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     _id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string,
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
 
+export default MovieCard;
